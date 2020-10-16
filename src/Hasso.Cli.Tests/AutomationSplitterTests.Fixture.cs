b00c2541e78416b1162f3458b2b8f940fb774a64
earloc <earloc@live.de>
2020-10-16 17:18:24 +0200

    implement automation-splitter

---
+++ src/Hasso.Cli.Tests/AutomationSplitterTests.Fixture.cs
@@ -0,0 +1,9 @@
+﻿using Hasso.Cli.Split;
+
+namespace Hasso.Cli.Tests.Scripts
+{
+    public class AutomationSplitterTestsFixture
+    {
+        public IAutomationSplitter SystemUnderTest { get; internal set; } = new YamlAutomationSplitter();
+    }
+}
---
+++ src/Hasso.Cli/Split/IAutomationSplitter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Hasso.Cli
+{
+    public interface IAutomationSplitter
+    {
+        public Task<IEnumerable<Fragment>> SplitAsync(string inputFileName)
+            => SplitAsync(new FileInfo(inputFileName));
+
+        public Task<IEnumerable<Fragment>> SplitAsync(FileInfo inputFile);
+
+    }
+}
