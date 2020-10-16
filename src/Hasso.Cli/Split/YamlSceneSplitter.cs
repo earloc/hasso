d23ca37561512aa48db363a3b4ac09fa8bf5f35b
earloc <earloc@live.de>
2020-10-16 16:25:29 +0200

    define interface and default implementation for scene-splitter

---
+++ src/Hasso.Cli/Split/YamlSceneSplitter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization;
+
+namespace Hasso.Cli.Split
+{
+    public class YamlSceneSplitter : ISceneSplitter
+    {
+        public Task<IEnumerable<Fragment>> SplitAsync(FileInfo inputFile)
+        {
+            var deserializer = new DeserializerBuilder().Build();
+            using var reader = inputFile.OpenText();
+
+            var content = deserializer.Deserialize(reader) as Dictionary<object, object>;
+
+            return Task.FromResult(
+                content.Select(
+                    _ => new Fragment { 
+                        Name = _.Key as string,
+                        Content = new Dictionary<object, object> { {_.Key, _.Value } }
+                    }
+                )
+            );
+        }
+    }
+}
